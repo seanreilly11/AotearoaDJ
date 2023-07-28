d2b4cc60500bbcc246174c79225c49eadd71d36f
Sean <seanreilly123@hotmail.com>
2023-07-28 18:06:34 +1200

    Icons and small style changes

--- client_web/src/components/CourseCard.jsx
+++ client_web/src/components/CourseCard.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import DetailPill from "./DetailPill";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faUserGraduate,
+    faUsers,
+    faUserGroup,
+    faVideo,
+} from "@fortawesome/free-solid-svg-icons";
 
 function CourseCard({ course }) {
     return (
@@ -12,10 +19,29 @@ function CourseCard({ course }) {
                 <div className="card">
                     <div className="card-body">
                         <h4 className="card-title">{course.title}</h4>
-                        <DetailPill content={course.difficulty} />
-                        <h6 className="mt-2">
-                            Students: {course.totalStudents}
-                        </h6>
+                        <div className="row align-items-center">
+                            {/* <div className="col-4">
+                                <DetailPill content={course.difficulty} />
+                            </div> */}
+                            <div className="col-4">
+                                <h6 className="mt-2">
+                                    <FontAwesomeIcon
+                                        icon={faUserGraduate}
+                                        style={{ marginRight: "2px" }}
+                                    />{" "}
+                                    {course.totalStudents}
+                                </h6>
+                            </div>
+                            <div className="col-4">
+                                <h6 className="mt-2">
+                                    <FontAwesomeIcon
+                                        icon={faVideo}
+                                        style={{ marginRight: "2px" }}
+                                    />{" "}
+                                    {course.videos.length}
+                                </h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </NavLink>
--- client_web/src/components/Sidebar.jsx
+++ client_web/src/components/Sidebar.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import logo from "../assets/images/aotearoa_dj_logo.png";
+import logo from "../assets/images/aotearoa_dj_logo.png";
 
 function Sidebar() {
     return (
         <div
-            className="d-flex flex-column flex-shrink-0 position-fixed p-3 text-bg-dark"
+            className="d-flex flex-column flex-shrink-0 position-fixed p-3 text-bg-dark sidebar"
             style={{ width: "280px", height: "100%" }}
         >
             <NavLink
                 to="/"
                 className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
             >
-                {/* <img src={logo} alt="" /> */}
-                <h1 className="fs-4">Aotearoa DJ Academy</h1>
+                <img src={logo} alt="" />
+                {/* <h1 className="fs-4">Aotearoa DJ Academy</h1> */}
             </NavLink>
             <hr />
             <ul className="nav nav-pills flex-column mb-auto">
                 <li className="nav-item">
                     <NavLink
                         to="/home"
-                        className="nav-link text-white"
+                        className="nav-link"
                         aria-current="page"
                     >
                         Home
                     </NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/course/1" className="nav-link text-white">
+                    <NavLink to="/course/1" className="nav-link">
                         Course
                     </NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/" className="nav-link text-white">
+                    <NavLink to="/" className="nav-link">
                         Log out
                     </NavLink>
                 </li>
